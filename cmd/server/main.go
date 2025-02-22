36e7c159da000076fc030318ab3b202b40b86c33
invinciblewest <invinciblewest@gmail.com>
2025-02-22 10:46:58 +0500

    iter1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,64 @@
 package main
 
-func main() {}
+import (
+	"github.com/invinciblewest/metrics/internal/storage"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	st := storage.NewMemStorage()
+
+	err := run(":8080", st)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func run(addr string, st storage.Storage) error {
+	mux := http.NewServeMux()
+
+	mux.Handle("/update/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		updateHandler(w, r, st)
+	}))
+
+	return http.ListenAndServe(addr, mux)
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request, st storage.Storage) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	explodedUrl := strings.Split(strings.Trim(r.URL.Path, "/update/"), "/")
+	if len(explodedUrl) != 3 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	metricType, metricName, metricValue := explodedUrl[0], explodedUrl[1], explodedUrl[2]
+
+	switch metricType {
+	case "gauge":
+		value, err := strconv.ParseFloat(metricValue, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		st.UpdateGauge(metricName, value)
+		return
+	case "counter":
+		value, err := strconv.ParseInt(metricValue, 10, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		st.UpdateCounter(metricName, value)
+		return
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
---
+++ internal/storage/memstorage.go
@@ -0,0 +1,25 @@
+package storage
+
+type MemStorage struct {
+	gauges   map[string]float64
+	counters map[string]int64
+}
+
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		gauges:   make(map[string]float64),
+		counters: make(map[string]int64),
+	}
+}
+
+func (st *MemStorage) UpdateGauge(name string, value float64) {
+	st.gauges[name] = value
+}
+
+func (st *MemStorage) UpdateCounter(name string, value int64) {
+	if _, exists := st.counters[name]; exists {
+		st.counters[name] += value
+	} else {
+		st.counters[name] = value
+	}
+}
