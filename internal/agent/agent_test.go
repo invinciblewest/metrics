e9dfccaebb4d97e515ce446c0432dbd02d2852ce
invinciblewest <invinciblewest@gmail.com>
2025-04-05 16:38:07 +0500

    iter10: mocks

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -1,44 +1,42 @@
 package agent
 
 import (
+	"github.com/golang/mock/gomock"
 	"github.com/invinciblewest/metrics/internal/agent/collectors"
+	"github.com/invinciblewest/metrics/internal/agent/collectors/mocks"
 	"github.com/invinciblewest/metrics/internal/agent/senders"
+	mocks2 "github.com/invinciblewest/metrics/internal/agent/senders/mocks"
 	"github.com/invinciblewest/metrics/internal/models"
-	"github.com/invinciblewest/metrics/internal/storage"
 	"github.com/invinciblewest/metrics/internal/storage/memstorage"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 )
 
-type MockCollector struct {
-	st storage.Storage
-}
-
-func (c *MockCollector) Collect() error {
-	delta := int64(1)
-	return c.st.UpdateCounter(models.Metric{
-		ID:    "PollCount",
-		MType: models.TypeCounter,
-		Delta: &delta,
-	})
-}
-
-type MockSender struct{}
-
-func (s *MockSender) SendMetric(metric models.Metric) error {
-	return nil
-}
-
 func TestNewAgent(t *testing.T) {
 	st := memstorage.NewMemStorage("", false)
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mCollector := mocks.NewMockCollector(ctrl)
+	mCollector.EXPECT().Collect().DoAndReturn(func() error {
+		delta := int64(1)
+		return st.UpdateCounter(models.Metric{
+			ID:    "PollCount",
+			MType: models.TypeCounter,
+			Delta: &delta,
+		})
+	}).AnyTimes()
+
+	mSender := mocks2.NewMockSender(ctrl)
+	mSender.EXPECT().SendMetric(gomock.Any()).Return(nil).AnyTimes()
+
 	collectorsList := []collectors.Collector{
-		&MockCollector{
-			st: st,
-		},
+		mCollector,
 	}
 	sendersList := []senders.Sender{
-		&MockSender{},
+		mSender,
 	}
 
 	go func() {
